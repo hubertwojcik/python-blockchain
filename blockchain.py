367bd86f218d815ede20d21a56510153e48246ee
Hubert Wójcik <hwojcikk@gmail.com>
2024-07-19 07:58:09 +0200

    create participants set

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,7 @@ genesis_block={
 blockchain=[genesis_block]
 open_transactions=[]
 owner='Hubert'
+participant={'Hubert'}
 
 def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
@@ -33,6 +34,8 @@ def add_transaction(recipient,sender=owner,amount=1.0):
         "amount":amount
     }
     open_transactions.append(transaction)
+    participant.add(sender)
+    participant.add(recipient)
     
     
 
@@ -82,6 +85,7 @@ while waiting_for_input:
     print("1: Add a new transaction value:")
     print("2: Mine block")
     print("3: Output the blockchain blocks")
+    print("4: Output participants")
     print("h: Manipulate the chain")
     print('q: Quit')
     user_choice=get_user_choice()
@@ -94,6 +98,8 @@ while waiting_for_input:
         mine_block()
     elif user_choice=='3':
         print_blockchain_elements()    
+    elif user_choice=='4':
+        print(participant)
     elif user_choice=='h':
         if len(blockchain)>=1:
             blockchain[0]={
